f1bf37a116294f00238e29fb61831c23d90bb832
n0f4ph4mst3r <vladredsoup@gmail.com>
2022-01-25 23:40:28 +0300

    add new task

---
+++ CppStudio/Normal/Task2/Task2/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+
+using namespace std;
+
+unsigned Input();
+
+int main() {
+	SetConsoleOutputCP(1251);
+
+    unsigned count = Input();
+
+    double fraction = 1;
+    for (unsigned i = 0; i < count; i++) {
+        fraction = 1 + 1 / fraction;
+    }
+
+    cout << fraction << endl;
+
+	system("pause");
+	return 0;
+}
+
+unsigned Input() {
+    unsigned value;
+    while (true) {
+        std::cout << "How much?: ";
+        std::cin >> value;
+
+        if (std::cin.fail()) {
+            cin.clear();
+            cin.ignore(UINT_MAX, '\n');
+            cout << "That input is invalid.\n";
+        }
+        else {
+            cin.ignore(UINT_MAX, '\n');
+            return value;
+        }
+    }
+}

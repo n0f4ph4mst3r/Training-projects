1761d2fcca9385b55d0e8882e5b434617f481655
Пользователь <vladredsoup@gmail.com>
2022-02-15 20:23:50 +0300

    add new task

---
+++ CppStudio/Normal/Task7/Task7/InputValue.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include <climits>
+#include <iostream>
+#include <cstdlib>
+#include <optional>
+#include <vector>
+
+//шаблонный класс для ввода данных в консоль
+template <class T> class InputValue {
+public:
+
+	InputValue() {};
+
+	void SetMax(T value) {
+		max = value;
+	};
+
+	void SetMessage(std::string str) {
+		msg = str;
+	}
+
+	T GetValue () {
+		Input();
+		return value;
+	};
+
+private:
+
+	std::string msg; //сообщение перед вводом значения
+
+	const T range; //максимально возможное для данного типа данных значение
+	std::optional<T> max = std::nullopt; //пользовательское ограничение для вводимого значения
+	T value;
+
+	void ClearInput() {
+		std::cin.clear();
+		std::cin.ignore(range, '\n');
+		std::cout << "That input is invalid.\n";
+	};
+
+	void Input() {
+		while (true) {
+			std::cout << msg;
+			std::cin >> value;
+
+			//если ввод не удался или значение не соотвествует ограничению -> очистить поток
+			if (std::cin.fail()) {
+				ClearInput();
+			}
+			else if (value > max && max != std::nullopt) {
+				ClearInput();
+			}
+			else {
+				std::cin.ignore(range, '\n');
+				break; //иначе прервать цикл
+			}
+		}
+	};
+
+
+};
+
+InputValue<int>::InputValue() : range(INT_MAX) {}
+InputValue<unsigned>::InputValue() : range(UINT_MAX) {}
+InputValue<char>::InputValue() : range(CHAR_MAX) {}
+
---
+++ CppStudio/Normal/Task7/Task7/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include "Windows.h"
+#include "InputValue.h"
+
+int main() {
+	SetConsoleOutputCP(1251);
+
+	InputValue<int>* inputINT = new InputValue<int>();
+	inputINT->SetMessage("Введите х: ");
+
+	int x = inputINT->GetValue();
+	double y = x;
+
+	while (true) {
+		double yNext = 0.5 * (y + 3 * x / (2 * pow(y,2) + x/y));
+		if (fabs(yNext - y) < pow(10, -5)) break;
+		else y = yNext;
+	}
+
+	std::cout << "Результат: " << y << std::endl;
+
+	system("pause");
+	return 0;
+}

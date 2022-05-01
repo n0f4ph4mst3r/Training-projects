39baa73533083fc3b35b95e3148bd9be83cddfec
Пользователь <vladredsoup@gmail.com>
2022-05-01 21:35:50 +0300

    add new task

---
+++ CppStudio/Normal/Task15/Task15/InputValue.h
@@ -0,0 +1,113 @@
+﻿#pragma once
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <sstream>
+
+//шаблонный класс для ввода данных в консоль
+template <class T> class InputValue {
+public:
+
+	InputValue() {};
+
+	void SetMax(T value) {
+		max = value;
+	};
+
+	void SetMessage(std::string str) {
+		msg = str;
+	}
+
+	void SetAllowedValues(std::vector<T> &values) {
+		allowedValues = std::move(values);
+	}
+	
+	T GetValue () {
+		Input();
+		return value;
+	};
+
+private:
+
+	std::string msg; //сообщение перед вводом значения
+
+	const size_t range; //максимально возможное для данного типа данных значение
+	std::optional<T> max = std::nullopt; //пользовательское ограничение для вводимого значения
+	std::vector<T> allowedValues; //допустимые значения
+	T value;
+
+	void ClearInput() {
+		std::cin.clear();
+		std::cin.ignore(range, '\n');
+		std::cout << "That input is invalid.\n";
+	};
+
+	void Input() {
+		while (true) {
+			std::cout << msg;
+			std::cin >> value;
+
+			//если ввод не удался или значение не соотвествует ограничению -> очистить поток
+			if (std::cin.fail()) {
+				ClearInput();
+			}
+			else {
+				if (max != std::nullopt && value > max) {
+					ClearInput();
+					continue;
+				}
+
+				if (allowedValues.size() > 0) {
+					for (size_t i = 0; i < allowedValues.size(); ++i) {
+						if (allowedValues[i] == value) {
+							std::cin.ignore(range, '\n');
+							return;
+						}
+					}
+
+					ClearInput();
+					continue;
+				}
+				
+				std::cin.ignore(range, '\n');
+				return;
+			}
+		}
+	};
+
+
+};
+
+InputValue<int>::InputValue() : range(INT_MAX) {}
+InputValue<unsigned>::InputValue() : range(UINT_MAX) {}
+InputValue<double>::InputValue() : range(DBL_MAX) {}
+InputValue<char>::InputValue() : range(CHAR_MAX) {}
+
+
+InputValue<std::string>::InputValue() : range(4294967295) {}
+void InputValue<std::string>::Input() {
+	while (true) {
+		std::cout << msg;
+		std::getline(std::cin >> std::ws, value);
+
+		//если ввод не удался или значение не соотвествует ограничению -> очистить поток
+		if (std::cin.fail()) {
+			ClearInput();
+		}
+		else {
+		    if (allowedValues.size() > 0) {
+				for (size_t i = 0; i < allowedValues.size(); ++i) {
+					if (allowedValues[i] == value) {
+						std::cin.ignore(range, '\n');
+						return;
+					}
+				}
+
+				ClearInput();
+				continue;
+			}
+
+			return;
+		}
+	}
+}
---
+++ CppStudio/Normal/Task15/Task15/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include <vector>
+#include <memory>
+#include <random>
+
+#include "InputValue.h"
+
+int main() {
+	SetConsoleOutputCP(1251);
+
+	std::unique_ptr<InputValue<unsigned>> InputUnsigned = std::make_unique<InputValue<unsigned>>();
+    
+	InputUnsigned->SetMessage("Высота матрицы: ");
+	unsigned height = InputUnsigned->GetValue();
+	InputUnsigned->SetMessage("Ширина матрицы: ");
+	unsigned width = InputUnsigned->GetValue();
+
+	std::vector<std::vector<int>> matrix;
+	matrix.resize(height);
+	for (auto &vec : matrix) vec.resize(width);
+
+	std::vector<int> vec;
+	std::cout << "Матрица:\r\n";
+	std::cout << std::endl;
+	for (unsigned i = 0; i < height; ++i) {
+		for (unsigned j = 0; j < width; ++j) {
+			std::random_device rd;     
+			std::mt19937 rng(rd());    
+			std::uniform_int_distribution<int> uni(-100, 100); 
+
+			matrix[i][j] = uni(rng);
+			std::cout << std::setw(6) << matrix[i][j];
+			vec.push_back(std::move(matrix[i][j]));
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "Вектор:\r\n";
+	for (auto elem : vec) std::cout << std::setw(6) << elem;
+	std::cout << std::endl;
+
+	system("pause");
+	return 0;
+}

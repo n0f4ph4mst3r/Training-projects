fd594763a93046663b88b63884072fc6314c0394
Пользователь <vladredsoup@gmail.com>
2022-07-06 16:14:20 +0300

    add new task

---
+++ CppStudio/Normal/Task18/Task18/InputValue.h
@@ -0,0 +1,115 @@
+﻿#pragma once
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <sstream>
+
+//шаблонный класс для ввода данных в консоль
+template <class T> class InputValue {
+public:
+	InputValue()  {};
+
+	void SetMax(const T value) {
+		m_max = value;
+	};
+
+	void SetMin(const T value) {
+		m_min = value;
+	};
+
+	void SetMessage(const std::string str) {
+		m_message = str;
+	}
+
+	void SetAllowedValues(const std::vector<T> &values) {
+		m_allowedValues = std::move(values);
+	}
+	
+	T GetValue () {
+		Input();
+		return m_value;
+	};
+
+private:
+	std::string m_message; //сообщение перед вводом значения
+	const size_t m_range; //максимально возможное для данного типа данных значение
+	std::optional<T> m_max = std::nullopt; //максимальное значенеи
+	std::optional<T> m_min = std::nullopt; //минимальное значение
+	std::vector<T> m_allowedValues; //допустимые значения
+	T m_value; //вводимое значение
+
+	void ClearInput() {
+		std::cin.clear();
+		std::cin.ignore(m_range, '\n');
+		std::cout << "That input is invalid.\n";
+	};
+
+	void Input() {
+		while (true) {
+			std::cout << m_message;
+			std::cin >> m_value;
+
+			//если ввод не удался или значение не соотвествует ограничению -> очистить поток
+			if (std::cin.fail()) {
+				ClearInput();
+			}
+			else {
+				if ((m_max != std::nullopt && m_value > m_max) || (m_min != std::nullopt && m_value < m_min)) {
+					ClearInput();
+					continue;
+				}
+
+				if (m_allowedValues.size() > 0) {
+					for (size_t i = 0; i < m_allowedValues.size(); ++i) {
+						if (m_allowedValues[i] == m_value) {
+							std::cin.ignore(m_range, '\n');
+							return;
+						}
+					}
+
+					ClearInput();
+					continue;
+				}
+				
+				std::cin.ignore(m_range, '\n');
+				return;
+			}
+		}
+	};
+
+
+};
+
+InputValue<int>::InputValue() : m_range(INT_MAX) {}
+InputValue<unsigned>::InputValue() : m_range(UINT_MAX) {}
+InputValue<double>::InputValue() : m_range((size_t)std::numeric_limits<double>::max) {}
+InputValue<char>::InputValue() : m_range(CHAR_MAX) {}
+
+
+InputValue<std::string>::InputValue() : m_range(4294967295) {}
+void InputValue<std::string>::Input() {
+	while (true) {
+		std::cout << m_message;
+		std::getline(std::cin >> std::ws, m_value);
+
+		//если ввод не удался или значение не соотвествует ограничению -> очистить поток
+		if (std::cin.fail()) {
+			ClearInput();
+		}
+		else {
+		    if (m_allowedValues.size() > 0) {
+				for (size_t i = 0; i < m_allowedValues.size(); ++i) {
+					if (m_allowedValues[i] == m_value) {
+						std::cin.ignore(m_range, '\n');
+						return;
+					}
+				}
+
+				ClearInput();
+				continue;
+			}
+
+			return;
+		}
+	}
+}
---
+++ CppStudio/Normal/Task18/Task18/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+#include <memory>
+#include <vector>
+
+#include "InputValue.h"
+
+int main() {
+	SetConsoleOutputCP(1251);
+
+	std::unique_ptr<InputValue<double>> value = std::make_unique<InputValue<double>>();
+	value->SetMin(0);
+	value->SetMessage("Введите сумму(руб.): ");
+	double sum = value->GetValue();
+	std::cout << std::endl;
+
+	std::unique_ptr<InputValue<unsigned>> option_input = std::make_unique<InputValue<unsigned>>();
+	option_input->SetMessage("Вы являетесь резидентом страны?\r\n1 - да\r\n2 - нет\r\n");
+	option_input->SetAllowedValues({ 1, 2 });
+	unsigned option = option_input->GetValue();
+	bool isResident = true;
+	if (option == 2) isResident = false;
+	std::cout << std::endl;
+
+	option_input->SetAllowedValues({ 1, 2, 3, 4, 5});
+	option_input->SetMessage("Каким образом получена сумма?\r\n1 - Трудовая деятельность\r\n2 - Продажа имущества\r\n3 - Дивиденты\r\n4 - Доход по вкладам\r\n5 - Приз или выигрыш\r\n");
+	option = option_input->GetValue();
+	std::cout << std::endl;
+
+	double proz;
+	switch (option) {
+	case 1: case 2:
+		if (isResident)
+			proz = 0.13;
+		else
+			proz = 0.3;
+		break;
+	case 3:
+		if (isResident)
+			proz = 0.13;
+		else
+			proz = 0.15;
+		break;
+	case 4: case 5:
+		proz = 0.35;
+		break;
+	}
+
+	std::cout << "Процентная ставка: " << proz * 100 << " %" << std::endl;
+	std::cout << "Налог НДФЛ: " << sum * proz << " руб." << std::endl;
+	std::cout << "Сумма на руки: " << sum - sum * proz << " руб." << std::endl;
+
+	system("pause");
+	return 0;
+}
